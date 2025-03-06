37eb8092044612de443de2531f2458c07940426b
Marcus Little <mcolelittle@gmail.com>
2025-03-06 13:27:46 -0500

    add back netlify-vary

--- middleware.js
+++ middleware.js
@@ -7,7 +7,11 @@ export function middleware(request) {
   const isMobile = /mobile|android|iphone|ipad|ipod/i.test(userAgent);
   const deviceType = isMobile ? 'mobile' : 'desktop';
   
+  // Set the Device-Type header
   response.headers.set('Device-Type', deviceType);
+  
+  // Add the Netlify-Vary header to create separate cache entries
+  response.headers.set('Netlify-Vary', 'header=Device-Type');
   
   const headerEntries = Array.from(response.headers.entries());
   const headerObject = Object.fromEntries(headerEntries);
