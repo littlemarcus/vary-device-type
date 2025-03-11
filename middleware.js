c4ec99ba6e45ee42b156795cc517918bebb51842
Marcus Little <mcolelittle@gmail.com>
2025-03-11 16:02:14 -0400

    set request header

--- middleware.js
+++ middleware.js
@@ -2,14 +2,14 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   const response = NextResponse.next();
+  const requestHeaders = new Headers(request.headers)
   
   const userAgent = request.headers.get('user-agent') || '';
   const isMobile = /mobile|android|iphone|ipad|ipod/i.test(userAgent);
   const deviceType = isMobile ? 'mobile' : 'desktop';
   
   // Set the Device-Type header
-  response.headers.set('Device-Type', deviceType);
-  
+  requestHeaders.set('Device-Type', deviceType);  
   // Add the Netlify-Vary header to create separate cache entries
   response.headers.set('Netlify-Vary', 'header=Device-Type');
   
